8263a9a0a4ab53005f45a821beeb213ef73deda3
PollyanaAGW <pollyanadias91@gmail.com>
2024-09-02 09:50:33 -0300

    ABCDFU and your mon

--- radioweb/app/context/HomeContext.tsx
+++ radioweb/app/context/HomeContext.tsx
@@ -1,8 +1,13 @@
-import {createContext} from 'react'
+'use client'
+
+import {createContext, ReactNode, useState} from 'react'
 
 type HomeContextData = {
     contador: number;
+    playing: boolean;
+    playing_texto: string;
     incremento: () => void;
+    togglePlaying: () => void;
 }
 
 export const HomeContext = createContext({} as HomeContextData);
