9daab6ed5b023c3069f7c560cf805440c096a075
PollyanaAGW <pollyanadias91@gmail.com>
2024-09-09 17:14:53 -0300

    arrumei o nome das musicas

--- radioweb/app/dados/musica.ts
+++ radioweb/app/dados/musica.ts
@@ -11,35 +11,35 @@ export const musics: Music[] = [
         nome: "Havana",
         author: "Camila Cabello",
         description: "Havana - Camila Cabello",
-        urlAudio: "audios/Camila Cabello - Havana (Audio) ft. Young Thug.mp3",
+        urlAudio: "audios/Havana.mp3",
         image: "Imagem da Música Havana"
     },
     {
         nome: "Shameless",
         author: "Camila Cabello",
         description: "Shameless - Camila Cabello",
-        urlAudio: "audios/Camila Cabello - Shameless (Official Video).mp3",
+        urlAudio: "audios/Shameless.mp3",
         image: "Imagem da Música Shameless"
     },
     {
         nome: "Not My Fault",
         author: "Reneé Rapp",
         description: "Not My Fault - Reneé Rapp",
-        urlAudio: "audios/Reneé Rapp, Megan Thee Stallion - Not My Fault (Official Music Video).mp3",
+        urlAudio: "audios/Not My Fault.mp3",
         image: "Imagem da Música Not My Fault"
     },
     {
         nome: "Taste",
         author: "Sabrina Carpenter",
         description: "Taste -  Sabrina Carpenter",
-        urlAudio: "audios/Sabrina Carpenter - Taste (Official Video).mp3",
+        urlAudio: "audios/Taste.mp3",
         image: "Imagem da Música Taste"
     },
     {
         nome: "Work From Home",
         author: "Fifth Harmony",
         description: "Work From Home - Fifth Harmony",
-        urlAudio: "audios/Fifth Harmony - Work from Home (Official Video) ft. Ty Dolla $ign.mp3",
+        urlAudio: "audios/Work from Home.mp3",
         image: "Imagem da Música Work Form Home"
     }
 ]
